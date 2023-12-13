cb14369d2eb9341ae08cfa9561657dd801053f70
Emanuele Simula <simulaemanuele@gmail.com>
2023-12-13 13:09:53 +0100

    exercise tipi primitivi in java - 1: done

---
+++ triangle/Main.java
@@ -0,0 +1,9 @@
+package triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        TriangleArea triangle = new TriangleArea();
+
+        System.out.println("Triangle area result: " + " " + triangle.calculateArea(5, 4));
+    }
+}
---
+++ triangle/TriangleArea.java
@@ -0,0 +1,11 @@
+package triangle;
+
+public class TriangleArea {
+
+    public Integer calculateArea(Integer base, Integer height) {
+        Integer triangleArea;
+        triangleArea = (base * height) / 2;
+        return triangleArea;
+    }
+
+}
